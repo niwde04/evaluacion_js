89fbc7ce7f4ba5532265c06d60b3e615484d4eea
Edwin Barahona <ed_barah@hotmail.com>
2020-01-06 14:47:39 -0600

    Initial commit

---
+++ codBase/js/app.js
@@ -0,0 +1,25 @@
+
+
+function init(){
+
+    var  uno =document.getElementById('suma');
+    var  dos =document.getElementById('suma');
+    var  tres =document.getElementById('suma');
+    var  cuatro =document.getElementById('suma');
+    var  cinco =document.getElementById('suma');
+    var  seis =document.getElementById('suma');
+    var  siete =document.getElementById('suma');
+    var  ocho  =document.getElementById('suma');
+    var  nueve =document.getElementById('suma');
+    var  suma =document.getElementById('suma');
+    var  suma =document.getElementById('suma');
+    var  suma =document.getElementById('suma');
+    var  suma =document.getElementById('suma');
+    var  suma =document.getElementById('suma');
+    var  suma =document.getElementById('suma');
+    var  suma =document.getElementById('suma');
+    var  suma =document.getElementById('suma');
+    var  suma =document.getElementById('suma');
+    var  suma =document.getElementById('suma');
+
+}
