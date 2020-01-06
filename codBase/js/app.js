45824b5bbfcd6daf3b1ca9d4aefb14f64059086e
Edwin Barahona <ed_barah@hotmail.com>
2020-01-06 16:03:56 -0600

    Agregar_el_punto
    ya funcionan todos los botones de numero solo falta agregar el punto y que solo se pueda agregar una vez

--- codBase/js/app.js
+++ codBase/js/app.js
@@ -2,24 +2,151 @@
 
 function init(){
 
-    var  uno =document.getElementById('suma');
-    var  dos =document.getElementById('suma');
-    var  tres =document.getElementById('suma');
-    var  cuatro =document.getElementById('suma');
-    var  cinco =document.getElementById('suma');
-    var  seis =document.getElementById('suma');
-    var  siete =document.getElementById('suma');
-    var  ocho  =document.getElementById('suma');
-    var  nueve =document.getElementById('suma');
-    var  suma =document.getElementById('suma');
-    var  suma =document.getElementById('suma');
-    var  suma =document.getElementById('suma');
-    var  suma =document.getElementById('suma');
-    var  suma =document.getElementById('suma');
-    var  suma =document.getElementById('suma');
-    var  suma =document.getElementById('suma');
-    var  suma =document.getElementById('suma');
-    var  suma =document.getElementById('suma');
-    var  suma =document.getElementById('suma');
+    var  cero =document.getElementById('0');
+    var  uno =document.getElementById('1');
+    var  dos =document.getElementById('2');
+    var  tres =document.getElementById('3');
+    var  cuatro =document.getElementById('4');
+    var  cinco =document.getElementById('5');
+    var  seis =document.getElementById('6');
+    var  siete =document.getElementById('7');
+    var  ocho  =document.getElementById('8');
+    var  nueve =document.getElementById('9');
+    var  resta =document.getElementById('menos');
+    var  multiplicacion =document.getElementById('por');
+    var  division =document.getElementById('dividido');
+    var  igual =document.getElementById('igual');
+    var  punto =document.getElementById('punto');
+    var  on =document.getElementById('on');
+    var  signo =document.getElementById('sign');
+ 
+    //eventos click
+
+        uno.onclick = function(e){
+           
+            if (display.textContent=='0'){
+                display.textContent = '';
+            }
+            if  (display.textContent.length <8){
+            display.textContent = display.textContent + "1";
+            } 
+        }
+
+        dos.onclick = function(e){
+           
+            if (display.textContent=='0'){
+                display.textContent = '';
+            }
+            if  (display.textContent.length <8){
+            display.textContent = display.textContent + "2";
+            }
+
+            
+        }
+        tres.onclick = function(e){
+           
+            if (display.textContent=='0'){
+                display.textContent = '';
+            }
+            if  (display.textContent.length <8){
+            display.textContent = display.textContent + "3";
+            }
+
+            
+        }
+        cuatro.onclick = function(e){
+           
+            if (display.textContent=='0'){
+                display.textContent = '';
+            }
+            if  (display.textContent.length <8){
+            display.textContent = display.textContent + "4";
+            }
+
+            
+        }
+        cinco.onclick = function(e){
+           
+            if (display.textContent=='0'){
+                display.textContent = '';
+            }
+            if  (display.textContent.length <8){
+            display.textContent = display.textContent + "5";
+            }
+
+            
+        }
+        seis.onclick = function(e){
+           
+            if (display.textContent=='0'){
+                display.textContent = '';
+            }
+            if  (display.textContent.length <8){
+            display.textContent = display.textContent + "6";
+            }
+
+            
+        }
+        siete.onclick = function(e){
+           
+            if (display.textContent=='0'){
+                display.textContent = '';
+            }
+            if  (display.textContent.length <8){
+            display.textContent = display.textContent + "7";
+            }
+
+            
+        }
+        ocho.onclick = function(e){
+           
+            if (display.textContent=='0'){
+                display.textContent = '';
+            }
+            if  (display.textContent.length <8){
+            display.textContent = display.textContent + "8";
+            }
+
+            
+        }
+        nueve.onclick = function(e){
+           
+            if (display.textContent=='0'){
+                display.textContent = '';
+            }
+            if  (display.textContent.length <8){
+            display.textContent = display.textContent + "9";
+            }
+
+            
+        }
+        cero.onclick = function(e){
+           
+            if (display.textContent=='0'){
+                display.textContent = '';
+            }
+            if  (display.textContent.length <8){
+            display.textContent = display.textContent + "0";
+            }
+
+            
+        }
+        on.onclick = function(e){
+           
+           
+            display.textContent =  "0";
+            
+
+            
+        }
+
+
+    
+     
+
+
+
+
+
 
 }
